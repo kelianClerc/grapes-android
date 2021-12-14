36b34a26fb0a7ccc7b6579ad375f6ed5d2ef1fdf
Kélian Clerc <kelian.clerc@fabernovel.com>
2021-12-14 15:43:21 +0100

    Add composable buttons

---
+++ compose-library/src/main/java/com/spendesk/grapes/compose/button/ButtonContentPadding.kt
@@ -0,0 +1,11 @@
+package com.spendesk.grapes.compose.button
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.unit.dp
+
+fun buttonContentPadding() = PaddingValues(
+    start = 16.dp,
+    top = 0.dp,
+    end = 16.dp,
+    bottom = 0.dp
+)
